a8ddb56dbd0fdfe7bcad9e52b6342625d26f4614
Bokai <bokai830124@gmail.com>
2020-05-10 21:02:27 +0800

    first commit

---
+++ app/Models/Animal.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Animal extends Model
+{
+    protected $fillable = [
+        'type_id',
+        'name',
+        'birthday',
+        'area',
+        'fix',
+        'description',
+        'personality',
+    ];
+}
